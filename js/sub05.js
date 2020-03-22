a2c989ee205d358e342d80e5066ab25a6a0900d4
lsh58 <60163978+lsh58@users.noreply.github.com>
2020-03-22 22:52:28 +0900

    .

--- js/sub05.js
+++ js/sub05.js
@@ -13,29 +13,30 @@ window.addEventListener('load', function () {
                 spanAll[i].style.display = 'none';
             }
             var span = this.childNodes;
-            span[0].style.display = 'block';
+            spanAll[k].style.display = 'block'; // 선택된애 밑에 삼각형 추가
+            
+            
+            if( k>=0 && k<=4){
+                console.log(k);
+                thumbnail[4].insertAdjacentHTML("afterend",'<div class="extended"><img src="../img/trailer/2019_01.jpg" alt=""></div>');
+            }
+            //넣어는주는데..거기에 바로 src 넣어주는 작업도 안되고, entend를 읽어야하는데 타이밍이 안맞는다..??
+            
             var url = this.style.backgroundImage;
-            console.log(lasturl);
-            if (url == lasturl) {
+
+            if (url == lasturl) { //같은애 클릭하면 닫히게
                 extend.style.display = 'none';
                 span[0].style.display = 'none';
                 url = null;
             }
             else {
                 extend.style.display = 'flex';
-                console.log(url.split('"')[1]);
+                extendimg.src = url.split('"')[1]; //url따와서 img에 src로 넣어주기
             }
             lasturl = url;
-
-
-            console.log(k);
-            // console.log(this.style.backgroundImage);
-
-            // if (extend.style.display == 'flex') {
-            //     
-            // } else {
-            //     
-            // }
+                     //X자 눌러도 닫게 구현,
+            //chevron 구현
+            //5단위로 div나오는곳 구현
         })
     }
 });
