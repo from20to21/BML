c8b1199911934cc95accf9dea34e583148cf2599
lsh58 <tmdgusdl58@gmail.com>
2020-03-31 13:00:43 +0900

    .

--- js/sub05.js
+++ js/sub05.js
@@ -139,3 +139,33 @@ scroll.addEventListener('click', function () {
     window.scrollTo({ top: 550, behavior: 'smooth' });
 })
 //스크롤이벤트end
+
+var headerTop = document.querySelector('.header__menu__mobile');
+var menuBtn = document.querySelector('.header__menu__mobile button');
+var menuOpen = document.querySelector('.header__menuContent__mobile');
+var menuBack = document.querySelector('.header__menuContent__background');
+
+menuBtn.addEventListener('click', function () {
+    headerTop.style.display = 'none';
+    menuOpen.style.left = '0';
+    menuBack.style.display = 'block';
+})
+menuBack.addEventListener('click', function () {
+    headerTop.style.display = 'block';
+    menuBack.style.display = 'none';
+    menuOpen.style.left = '-100%';
+})
+
+var lastScroll = 0;
+window.addEventListener('scroll', function () {
+    var scroll = window.scrollY;
+
+    if (scroll > lastScroll) {
+        headerTop.style.display = 'none';
+    }
+    else {
+        headerTop.style.display = 'block';
+    }
+    if (scroll <= 0) { headerTop.style.display = 'block' }
+    lastScroll = scroll;
+});
